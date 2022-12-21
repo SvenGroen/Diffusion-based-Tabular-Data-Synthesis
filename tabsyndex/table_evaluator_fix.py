deaa7f71edb13506f3a6223726ecfd87eb81fdfb
SvenGroen <Sven.Groen@studium.uni-hamburg.de>
2022-12-21 11:38:12 +0100

    table_evaluator fix

--- tabsyndex/table_evaluator_fix.py
+++ tabsyndex/table_evaluator_fix.py
@@ -20,7 +20,38 @@ class TableEvaluatorFix(TableEvaluator):
         plot_correlation_difference(self.real, self.fake, cat_cols=self.categorical_columns, plot_diff=plot_diff, fname=fname,
                                     **kwargs)
 
+    def plot_cumsums(self, nr_cols=4, fname=None):
+        """
+        Plot the cumulative sums for all columns in the real and fake dataset. Height of each row scales with the length of the labels. Each plot contains the
+        values of a real columns and the corresponding fake column.
+        :param fname: If not none, saves the plot with this file name. 
+        """
+        nr_charts = len(self.real.columns)
+        nr_rows = max(1, nr_charts // nr_cols)
+        nr_rows = nr_rows + 1 if nr_charts % nr_cols != 0 else nr_rows
 
+        max_len = 0
+        # Increase the length of plots if the labels are long
+        if not self.real.select_dtypes(include=['object']).empty:
+            lengths = []
+            for d in self.real.select_dtypes(include=['object']):
+                lengths.append(max([len(x.strip()) for x in self.real[d].unique().tolist()]))
+            max_len = max(lengths)
+
+        row_height = 6 + (max_len // 30)
+        fig, ax = plt.subplots(nr_rows, nr_cols, figsize=(16, row_height * nr_rows))
+        fig.suptitle('Cumulative Sums per feature', fontsize=16)
+        axes = ax.flatten()
+        for i, col in enumerate(self.real.columns):
+            r = self.real[col]
+            f = self.fake.iloc[:, self.real.columns.tolist().index(col)]
+            cdf(r, f, col, 'Cumsum', ax=axes[i])
+        plt.tight_layout(rect=[0, 0.02, 1, 0.98])
+
+        if fname is not None: 
+            plt.savefig(fname)
+
+        plt.show()
     
     def visual_evaluation(self, save_dir=None, **kwargs):
         """
@@ -30,7 +61,7 @@ class TableEvaluatorFix(TableEvaluator):
         """
         if save_dir is None: 
             super().plot_mean_std()
-            super().plot_cumsums()
+            self.plot_cumsums()
             super().plot_distributions()
             self.plot_correlation_difference(**kwargs)
             super().plot_pca()    
@@ -39,7 +70,7 @@ class TableEvaluatorFix(TableEvaluator):
             save_dir.mkdir(parents=True, exist_ok=True)
 
             super().plot_mean_std(fname=save_dir/'mean_std.png')
-            super().plot_cumsums(fname=save_dir/'cumsums.png')
+            self.plot_cumsums(fname=save_dir/'cumsums.png')
             super().plot_distributions(fname=save_dir/'distributions.png')
             self.plot_correlation_difference(fname=save_dir/'correlation_difference.png', **kwargs)
             super().plot_pca(fname=save_dir/'pca.png') 
@@ -105,4 +136,41 @@ def plot_correlation_difference(real: pd.DataFrame, fake: pd.DataFrame, plot_dif
         if fname is not None: 
             plt.savefig(fname)
 
+        plt.show()
+
+def cdf(data_r, data_f, xlabel: str = 'Values', ylabel: str = 'Cumulative Sum', ax=None):
+    """
+    Plot continous density function on optionally given ax. If no ax, cdf is plotted and shown.
+
+    :param data_r: Series with real data
+    :param data_f: Series with fake data
+    :param xlabel: Label to put on the x-axis
+    :param ylabel: Label to put on the y-axis
+    :param ax: The axis to plot on. If ax=None, a new figure is created.
+    """
+    x1 = np.sort(data_r)
+    x2 = np.sort(data_f)
+    y = np.arange(1, len(data_r) + 1) / len(data_r)
+
+    ax = ax if ax else plt.subplots()[1]
+
+    axis_font = {'size': '14'}
+    ax.set_xlabel(xlabel, **axis_font)
+    ax.set_ylabel(ylabel, **axis_font)
+
+    ax.grid()
+    ax.plot(x1, y, marker='o', linestyle='none', label='Real', ms=8)
+    ax.plot(x2, y, marker='o', linestyle='none', label='Fake', alpha=0.5)
+    ax.tick_params(axis='both', which='major', labelsize=8)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=3)
+    import matplotlib.ticker as mticker
+
+    # If labels are strings, rotate them vertical
+    if isinstance(data_r, pd.Series) and data_r.dtypes == 'object':
+        ticks_loc = ax.get_xticks()
+        ticks_loc = ticks_loc[:len(data_r.sort_values().unique())]# MANUAL FIX --> not in original library
+        ax.xaxis.set_major_locator(mticker.FixedLocator(ticks_loc))
+        ax.set_xticklabels(data_r.sort_values().unique(), rotation='vertical')
+
+    if ax is None:
         plt.show()
